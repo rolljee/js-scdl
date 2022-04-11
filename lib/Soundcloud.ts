9697476f0f50d9273eca6665926fa64fa8dc60ef
Nicolas <n.riquelmebareiro@gmail.com>
2022-04-11 10:20:48 +0200

    fix deps

--- lib/Soundcloud.ts
+++ lib/Soundcloud.ts
@@ -1,34 +1,32 @@
 import cp from 'child_process';
 
 export class SoundCloud {
-	constructor() {}
+  guard(url = '', outputFolder = ''): void | Error {
+    if (outputFolder.charAt(0) === '~') {
+      // path must be absolute for spawn to work.
+      throw new Error('You must use absolute path');
+    }
 
-	guard(url = '', outputFolder = ''): void | Error {
-		if (outputFolder.charAt(0) === '~') {
-			// path must be absolute for spawn to work.
-			throw new Error('You must use absolute path');
-		}
+    if (!url.includes('soundcloud')) {
+      throw new Error('Wrong url ?');
+    }
+  }
 
-		if (!url.includes('soundcloud')) {
-			throw new Error('Wrong url ?');
-		}
-	}
+  download(url = '', outputFolder = ''): cp.ChildProcessWithoutNullStreams | Error {
+    this.guard(url, outputFolder);
 
-	download(url = '', outputFolder = ''): cp.ChildProcessWithoutNullStreams | Error {
-		this.guard(url, outputFolder);
+    return cp.spawn('scdl', ['-l', url, '--path', `${outputFolder}`, '-c']);
+  }
 
-		return cp.spawn('scdl', ['-l', url, '--path', `${outputFolder}`, '-c']);
-	}
+  downloadAll(url = '', outputFolder = '', user = ''): cp.ChildProcessWithoutNullStreams | Error {
+    this.guard(url, outputFolder);
 
-	downloadAll(url = '', outputFolder = '', user = ''): cp.ChildProcessWithoutNullStreams | Error {
-		this.guard(url, outputFolder);
+    return cp.spawn('scdl', ['-a', url, '--path', `${outputFolder}`, user, '-c']);
+  }
 
-		return cp.spawn('scdl', ['-a', url, '--path', `${outputFolder}`, user, '-c']);
-	}
+  downloadPlaylist(url = '', outputFolder = '', user = ''): cp.ChildProcessWithoutNullStreams | Error {
+    this.guard(url, outputFolder);
 
-	downloadPlaylist(url = '', outputFolder = '', user = ''): cp.ChildProcessWithoutNullStreams | Error {
-		this.guard(url, outputFolder);
-
-		return cp.spawn('scdl', [url, '--path', `${outputFolder}`, user, '-c', '-p']);
-	}
+    return cp.spawn('scdl', [url, '--path', `${outputFolder}`, user, '-c', '-p']);
+  }
 }
